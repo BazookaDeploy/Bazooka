918305535ec3cb5c957eab0c734edb794c638361
Paolo Nicodemo <paolonicodemo@gmail.com>
2014-12-15 23:32:49 +0100

    removed bower in favor of webpack

---
+++ Sources/Web/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  cache: true,
+  entry: './App/index',
+  output: {
+    filename: './App/browser-bundle.js'
+  },
+  module: {
+    loaders: [
+    {test: /\.js$/, loader: 'jsx-loader'}
+    ]
+  }
+};
