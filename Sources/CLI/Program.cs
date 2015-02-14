fb66c67505fb6e7081971e7a08461fc05314a7a3
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-02-14 20:22:07 +0100

    added update option

--- Sources/CLI/Program.cs
+++ Sources/CLI/Program.cs
@@ -93,6 +93,64 @@ namespace Bazooka.CLI
 
                     }
                     break;
+                case "update":
+                    {
+                        //serach directory for packages
+                        var files = Directory.GetFiles(((BlastOptions)invokedVerbInstance).Directory, "*.nupkg");
+
+                        if (files.Count() == 0)
+                        {
+                            Console.WriteLine("No package found");
+                            break;
+                        }
+
+
+                        if (files.Count() > 1)
+                        {
+                            Console.WriteLine("More than one package present");
+                            break;
+                        }
+
+                        var packageInfo = new PackageInfo();
+                        packageInfo.Name = PackageHelpers.ExtractPackageName(files.First());
+                        packageInfo.Version = PackageHelpers.ExtractPackageVersion(files.First());
+                        packageInfo.InstallationDirectory = ((UpdateOptions)invokedVerbInstance).Directory;
+                        packageInfo.Configuration = ((UpdateOptions)invokedVerbInstance).Configuration;
+
+                        var packageRemover = new PackageRemover();
+                        packageRemover.Logger = new ConsoleLogger();
+
+                        var additionalParams = ((UpdateOptions)invokedVerbInstance).AdditionalParameters;
+                        var paramDictionary = new Dictionary<string, string>();
+
+                        if (additionalParams.Length > 0)
+                        {
+                            paramDictionary = additionalParams.ToDictionary(x => x.Substring(0, x.IndexOf("=") - 1), x => x.Substring(x.IndexOf("=") + 1));
+                        }
+
+                        packageRemover.Remove(packageInfo, new string[] { ((UpdateOptions)invokedVerbInstance).Directory }, paramDictionary);
+
+                        var packageInfo2 = new PackageInfo();
+
+                        packageInfo2.Configuration = ((UpdateOptions)invokedVerbInstance).Configuration;
+                        packageInfo2.InstallationDirectory = ((UpdateOptions)invokedVerbInstance).Directory;
+                        packageInfo2.Name = ((UpdateOptions)invokedVerbInstance).Application;
+                        packageInfo2.Version = ((UpdateOptions)invokedVerbInstance).Version;
+
+                        var PackageInstaller = new PackageInstaller();
+                        PackageInstaller.Logger = new ConsoleLogger();
+                        var additionalParams2 = ((UpdateOptions)invokedVerbInstance).AdditionalParameters;
+                        var paramDictionary2 = new Dictionary<string, string>();
+
+                        if (additionalParams2.Length > 0)
+                        {
+                            paramDictionary2 = additionalParams2.ToDictionary(x => x.Substring(0, x.IndexOf("=") - 1), x => x.Substring(x.IndexOf("=") + 1));
+                        }
+
+                        PackageInstaller.Install(packageInfo2, new string[] { ((UpdateOptions)invokedVerbInstance).Repository }, paramDictionary2);
+
+                    }
+                    break;
                 case "uninstall":
                     {
                         var packageInfo = new PackageInfo();
