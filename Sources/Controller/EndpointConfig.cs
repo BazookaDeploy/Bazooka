cd61befeed8093ff75da0b3027cf5216c4062555
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-02-20 23:03:09 +0100

    Began implementation of deployment handler

--- Sources/Controller/EndpointConfig.cs
+++ Sources/Controller/EndpointConfig.cs
@@ -1,6 +1,11 @@
 
 namespace Controller
 {
+    using DataAccess.Write;
+    using NHibernate;
+    using NHibernate.Cfg;
+    using NHibernate.Dialect;
+    using NHibernate.Mapping.ByCode;
     using NServiceBus;
     using NServiceBus.Persistence;
     using NServiceBus.Persistence.Legacy;
@@ -12,8 +17,30 @@ namespace Controller
 	*/
     public class EndpointConfig : IConfigureThisEndpoint
     {
+        public static ISessionFactory Store { get; set; }
+
+
         public void Customize(BusConfiguration configuration)
         {
+            var config = new NHibernate.Cfg.Configuration();
+            config.DataBaseIntegration(db =>
+            {
+                db.Dialect<MsSql2008Dialect>();
+                db.ConnectionStringName = "DataContext";
+            });
+
+            var mapper = new ModelMapper();
+            mapper.AddMapping<ApplicationMap>();
+            mapper.AddMapping<EnviromentMap>();
+            mapper.AddMapping<DeployUnitMap>();
+            mapper.AddMapping<ParameterMap>();
+            mapper.AddMapping<DeploymentMap>();
+
+            config.AddMapping(mapper.CompileMappingForAllExplicitlyAddedEntities());
+
+            Store = config.BuildSessionFactory();
+
+
             configuration.UsePersistence<MsmqPersistence>() ;
             configuration.EndpointName("bazooka.controller");
             configuration.UseSerialization<JsonSerializer>();
