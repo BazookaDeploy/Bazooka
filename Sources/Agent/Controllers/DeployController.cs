38e105cc3980ff554527a4b353c7e888f0a9cda6
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-02-22 00:02:43 +0100

    Completed deploy handler

--- Sources/Agent/Controllers/DeployController.cs
+++ Sources/Agent/Controllers/DeployController.cs
@@ -3,7 +3,9 @@
     using Bazooka.Core;
     using Bazooka.Core.Dto;
     using System;
+    using System.Collections;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -11,6 +13,76 @@
 
     public class DeployController : ApiController
     {
+        [HttpPost]
+        public ExecutionResult Update(InstallDto installOptions)
+        {
+            var logger = new StringLogger();
+            try
+            {
+                var files = Directory.GetFiles(installOptions.Directory, "*.nupkg");
+
+                if (files.Count() == 0)
+                {
+                    return new ExecutionResult()
+                    {
+                        Success = false,
+                        Exception = "no package found",
+                        Log = logger.Logs
+                    };
+                }
+
+
+                if (files.Count() > 1)
+                {
+                    return new ExecutionResult()
+                    {
+                        Success = false,
+                        Exception = "multiple packages found",
+                        Log = logger.Logs
+                    };
+                }
+
+                var packageInfo = new PackageInfo();
+                var filename = PackageHelpers.ExtractPackageName(files.First()).Trim();
+                packageInfo.Name = filename.Substring(filename.LastIndexOf("\\") + 1);
+                packageInfo.Version = PackageHelpers.ExtractPackageVersion(files.First()).Trim();
+                packageInfo.InstallationDirectory = installOptions.Directory;
+                packageInfo.Configuration = installOptions.Configuration;
+
+                var packageRemover = new PackageRemover();
+                packageRemover.Logger = logger;
+
+                var additionalParams = installOptions.AdditionalParameters;
+                packageRemover.Remove(packageInfo, new string[] { installOptions.Directory }, additionalParams);
+
+
+
+                var packageInfo2 = new PackageInfo();
+                packageInfo2.Configuration = installOptions.Configuration;
+                packageInfo2.InstallationDirectory = installOptions.Directory;
+                packageInfo2.Name = installOptions.Application;
+                packageInfo2.Version = installOptions.Version;
+                var PackageInstaller = new PackageInstaller();
+                PackageInstaller.Logger = logger;
+                PackageInstaller.Install(packageInfo2, new string[] { installOptions.Repository }, installOptions.AdditionalParameters);
+                return new ExecutionResult()
+                {
+                    Success = true,
+                    Log = logger.Logs
+                };
+            }
+            catch (Exception e)
+            {
+                return new ExecutionResult()
+                {
+                    Success = false,
+                    Exception = e.Message,
+                    Log = logger.Logs
+                };
+            }
+        }
+
+
         [HttpPost]
         public ExecutionResult Install(InstallDto installOptions)
         {
---
+++ Sources/Controller/DeployApplicationHandler.cs
@@ -0,0 +1,158 @@
+﻿namespace Controller
+{
+    using Bazooka.Core.Commands;
+    using Bazooka.Core.Dto;
+    using DataAccess.Read;
+    using DataAccess.Write;
+    using Newtonsoft.Json;
+    using NServiceBus;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+
+    public class DeployApplicationHandler : IHandleMessages<DeployApplication>
+    {
+        public void Handle(DeployApplication message)
+        {
+            int envId;
+            string version;
+            string config;
+            using (var session = EndpointConfig.Store.OpenSession())
+            {
+                var dep = session.Load<Deployment>(message.DeploymentId);
+                dep.StartDate = DateTime.UtcNow;
+                dep.Status = Status.Running;
+                version = dep.Version;
+                dep.Log = "Deploy started";
+                envId = dep.EnviromentId;
+                session.Update(dep);
+                session.Flush();
+            }
+
+            using (var dc = new ReadContext())
+            {
+                config = dc.Enviroments.Single(x => x.Id == envId).Configuration;
+                var units = dc.DeployUnits.Where(x => x.EnviromentId == envId).ToList();
+
+                foreach (var unit in units)
+                {
+                    try
+                    {
+                        var res = new List<string>();
+                        if (unit.CurrentlyDeployedVersion == null)
+                        {
+                            var ret = Update(unit, version, config);
+                            using (var session = EndpointConfig.Store.OpenSession())
+                            {
+                                var dep = session.Load<Deployment>(message.DeploymentId);
+                                foreach (var mess in ret)
+                                {
+                                    dep.Log += mess;
+                                }
+                                session.Update(dep);
+                                session.Flush();
+                            }
+                        }
+                        else
+                        {
+                            var ret = Install(unit, version, config);
+                            using (var session = EndpointConfig.Store.OpenSession())
+                            {
+                                var dep = session.Load<Deployment>(message.DeploymentId);
+                                foreach (var mess in ret)
+                                {
+                                    dep.Log += mess;
+                                }
+                                session.Update(dep);
+                                session.Flush();
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        using (var session = EndpointConfig.Store.OpenSession())
+                        {
+                            var dep = session.Load<Deployment>(message.DeploymentId);
+                            dep.EndDate = DateTime.UtcNow;
+                            dep.Status = Status.Failed;
+                            dep.Log += e.Message;
+                            session.Update(dep);
+                            session.Flush();
+                        }
+
+                        return;
+                    }
+
+                    using (var session = EndpointConfig.Store.OpenSession())
+                    {
+                        var deployUnit = session.Load<DeployUnit>(unit.Id);
+                        deployUnit.CurrentlyDeployedVersion = version;
+                        session.Update(deployUnit);
+                        session.Flush();
+                    }
+                }
+
+            }
+
+            using (var session = EndpointConfig.Store.OpenSession())
+            {
+                var dep = session.Load<Deployment>(message.DeploymentId);
+                dep.EndDate = DateTime.UtcNow;
+                dep.Status = Status.Ended;
+                dep.Log += "Deploy ended";
+                session.Update(dep);
+                session.Flush();
+            }
+        }
+
+
+
+
+
+        private ICollection<string> Install(DeployUnitDto unit, string version, string config)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(unit.Machine);
+
+                var result = client.PostAsJsonAsync("/api/deploy/install", new
+                {
+                    Application = unit.PackageName,
+                    Version = version,
+                    Directory = unit.Directory,
+                    Configuration = config,
+                    Repository = unit.Repository,
+                    AdditionalParameters = unit.Parameters
+                }).Result;
+
+
+                var response = JsonConvert.DeserializeObject<ExecutionResult>(result.Content.ReadAsStringAsync().Result);
+                return new string[] { response.Log };
+            }
+        }
+
+
+        private ICollection<string> Update(DeployUnitDto unit, string version, string config)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(unit.Machine);
+
+                var result = client.PostAsJsonAsync("/api/deploy/update", new
+                {
+                    Application = unit.PackageName,
+                    Version = version,
+                    Directory = unit.Directory,
+                    Configuration = config,
+                    Repository = unit.Repository,
+                    AdditionalParameters = unit.Parameters
+                }).Result;
+
+
+                var response = JsonConvert.DeserializeObject<ExecutionResult>(result.Content.ReadAsStringAsync().Result);
+                return new string[] { response.Log };
+            }
+        }
+    }
+}
