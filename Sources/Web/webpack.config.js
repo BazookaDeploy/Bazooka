a916ca7999f96900001f27d84126b4deea348574
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-06-27 17:05:41 +0200

    removed jsxloader in favor of babel

--- Sources/Web/webpack.config.js
+++ Sources/Web/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ],
     module: {
         loaders: [
-        { test: /\.js$/, loader: 'jsx-loader?harmony', exclude: /node_modules/ }
+        { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
         ]
     }
 };
