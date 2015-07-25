10fea1ad0a1cab51ada0b1cf364a20894a8893dc
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-07-25 22:30:50 +0200

    Added remote script task table

--- Database/Database/Write/LocalScriptTasks.sql
+++ Database/Database/Write/LocalScriptTasks.sql
@@ -2,6 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[Script] NVARCHAR(MAX) NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
 	[EnviromentId] INT NOT NULL, 
     CONSTRAINT [FK_LocalScriptTasks_ToEnviroments] FOREIGN KEY ([EnviromentId]) REFERENCES [Enviroments]([Id])
 )
---
+++ Database/Database/Write/RemoteScriptTasks.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[RemoteScriptTasks]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Script] NVARCHAR(MAX) NOT NULL,	
+    [Machine] NVARCHAR(50) NOT NULL,	
+	[Name] NVARCHAR(50) NOT NULL,
+	[EnviromentId] INT NOT NULL, 
+    CONSTRAINT [FK_RemoteScriptTasks_ToEnviroments] FOREIGN KEY ([EnviromentId]) REFERENCES [Enviroments]([Id])
+)
