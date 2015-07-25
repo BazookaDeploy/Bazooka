26557d355e7b6c34499f6c5158eec0777a3f5238
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-07-25 22:27:55 +0200

    added table for local scripts task

---
+++ Database/Database/Write/LocalScriptTasks.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[LocalScriptTasks]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Script] NVARCHAR(MAX) NOT NULL,
+	[EnviromentId] INT NOT NULL, 
+    CONSTRAINT [FK_LocalScriptTasks_ToEnviroments] FOREIGN KEY ([EnviromentId]) REFERENCES [Enviroments]([Id])
+)
