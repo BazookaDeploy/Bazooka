53a9665fe0f62ddf426237b5411b000e5b6d0127
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-07-25 22:34:18 +0200

    added mail tasks table

---
+++ Database/Database/Write/MailTasks.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[MailTasks]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Text] NVARCHAR(MAX) NOT NULL,
+	[Recipients] NVARCHAR(256) NOT NULL,
+	[EnviromentId] INT NOT NULL, 
+    CONSTRAINT [FK_MailTasks_ToEnviroments] FOREIGN KEY ([EnviromentId]) REFERENCES [Enviroments]([Id])
+
+)
