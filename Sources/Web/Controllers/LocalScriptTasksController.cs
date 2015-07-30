37603f89dee2268e3dce6afaf21005354386d53a
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-07-30 22:46:13 +0200

    added scripts controllers

---
+++ Sources/Web/Controllers/LocalScriptTasksController.cs
@@ -0,0 +1,45 @@
+﻿namespace Web.Controllers
+{
+    using DataAccess.Read;
+    using DataAccess.Write;
+    using System.Linq;
+    using System.Web.Http;
+
+    public class LocalScriptTasksController : ApiController
+    {
+        private ReadContext db = new ReadContext();
+
+        public LocalScriptTaskDto Get(int id)
+        {
+            return db.LocalScriptTasks.Single(x => x.Id == id);
+        }
+
+        public void Put(LocalScriptTask unit)
+        {
+            using (var session = WebApiApplication.Store.OpenSession())
+            {
+                session.SaveOrUpdate(unit);
+                session.Flush();
+            };
+        }
+
+        public void Post(LocalScriptTask unit)
+        {
+            using (var session = WebApiApplication.Store.OpenSession())
+            {
+                session.SaveOrUpdate(unit);
+                session.Flush();
+            };
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+
---
+++ Sources/Web/Controllers/RemoteScriptTasksController.cs
@@ -0,0 +1,44 @@
+﻿namespace Web.Controllers
+{
+    using DataAccess.Read;
+    using DataAccess.Write;
+    using System.Linq;
+    using System.Web.Http;
+
+    public class RemoteScriptTasksController : ApiController
+    {
+        private ReadContext db = new ReadContext();
+
+        public RemoteScriptTaskDto Get(int id)
+        {
+            return db.RemoteScriptTasks.Single(x => x.Id == id);
+        }
+
+        public void Put(RemoteScriptTask unit)
+        {
+            using (var session = WebApiApplication.Store.OpenSession())
+            {
+                session.SaveOrUpdate(unit);
+                session.Flush();
+            };
+        }
+
+        public void Post(RemoteScriptTask unit)
+        {
+            using (var session = WebApiApplication.Store.OpenSession())
+            {
+                session.SaveOrUpdate(unit);
+                session.Flush();
+            };
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
