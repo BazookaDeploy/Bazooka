23b6f92e85b3c002911ebcd6295de54a8c3fd976
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-08-10 22:43:36 +0200

    added log compaction job to clean old logs

---
+++ Sources/Jobs/LogsCompactonJob.cs
@@ -0,0 +1,59 @@
+﻿namespace Jobs
+{
+    using DataAccess.Read;
+    using DataAccess.Write;
+    using NHibernate;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LogsCompactonJob
+    {
+        /// <summary>
+        ///     Nhibernate store to use to save the data    
+        /// </summary>
+        public static ISessionFactory Store { get; set; }
+
+        public static void Execute()
+        {
+            List<int> deploys;
+
+            using (var db = new ReadContext())
+            {
+                deploys = db.Deployments
+                            .Where(x => x.Status == DataAccess.Write.Status.Ended)
+                            .Where(x => x.StartDate < DateTime.UtcNow.AddDays(-30))
+                            .Where(x => x.Logs.Count > 1)
+                            .Select(x => x.Id)
+                            .ToList();
+            }
+
+            foreach (var deploy in deploys)
+            {
+                using (var session = Store.OpenSession())
+                {
+                    using (var db = new ReadContext())
+                    {
+                        var current = db.Deployments
+                                    .Single(x => x.Id == deploy);
+
+                        foreach (var log in current.Logs)
+                        {
+                            var logLine = session.Load<LogEntry>(log.Id);
+                            session.Delete(logLine);
+                        }
+
+                        session.Save(new LogEntry()
+                        {
+                            DeploymentId = deploy,
+                            Text = "Logs removed due to compaction",
+                            TimeStamp = DateTime.UtcNow
+                        });
+
+                        session.Flush();
+                    }
+                }
+            }
+        }
+    }
+}
