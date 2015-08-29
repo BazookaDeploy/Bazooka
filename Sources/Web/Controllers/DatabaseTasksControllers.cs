7630bff53f3741a5447cf453716736f8dbecdec3
Paolo Nicodemo <paolonicodemo@gmail.com>
2015-08-29 16:41:20 +0200

    added controller

---
+++ Sources/Web/Controllers/DatabaseTasksControllers.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess.Read;
+using DataAccess.Write;
+using System.Linq;
+using System.Web.Http;
+
+namespace Web.Controllers
+{
+    public class DatabaseTasksControllers : ApiController
+    {
+        private ReadContext db = new ReadContext();
+
+        public DatabaseTaskDto Get(int id)
+        {
+            return db.DatabaseTasks.Single(x => x.Id == id);
+        }
+
+        public void Put(DatabaseTask unit)
+        {
+            using (var session = WebApiApplication.Store.OpenSession())
+            {
+                session.SaveOrUpdate(unit);
+                session.Flush();
+            };
+        }
+
+        public void Post(DatabaseTask unit)
+        {
+            using (var session = WebApiApplication.Store.OpenSession())
+            {
+                session.SaveOrUpdate(unit);
+                session.Flush();
+            };
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
