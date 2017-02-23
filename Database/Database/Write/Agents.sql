4658f7e3e92261f1acc78830210409870a2e79e8
Paolo Nicodemo <paolonicodemo@gmail.com>
2017-02-23 22:39:34 +0100

    added agent status field to database

--- Database/Database/Write/Agents.sql
+++ Database/Database/Write/Agents.sql
@@ -4,5 +4,7 @@
     [Name] NVARCHAR(50) NOT NULL, 
     [Address] NVARCHAR(50) NOT NULL, 
     [EnviromentId] INT NOT NULL, 
+    [LastStatusCheck] DATETIME NULL DEFAULT NULL, 
+    [LastCheck] BIT NOT NULL DEFAULT 0, 
     CONSTRAINT [FK_Agents_ToEnviromens] FOREIGN KEY ([EnviromentId]) REFERENCES [Enviroments]([Id])
 )
