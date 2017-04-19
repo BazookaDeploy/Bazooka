1716cef93cf530b13c53942a6381d241a960d60a
Paolo Nicodemo <paolonicodemo@gmail.com>
2017-04-19 23:22:14 +0200

    Added write classes

---
+++ Sources/DataAccess/Write/TaskTemplate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccess.Write
+{
+    /// <summary>
+    ///     A template for a task
+    /// </summary>
+    public class TaskTemplate
+    {
+        /// <summary>
+        ///     Template identifier
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        ///     name of the template
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary>
+        ///     An optional description of the template
+        /// </summary>
+        public virtual string Description { get; set; }
+
+        /// <summary>
+        ///     The powershell script to execute the template
+        /// </summary>
+        public virtual string Script { get; set; }
+
+        /// <summary>
+        ///     List of parameters
+        /// </summary>
+        public virtual IList<TaskTemplateParameter> Parameters { get; set; }
+    }
+}
---
+++ Sources/DataAccess/Write/TaskTemplateParameter.cs
@@ -0,0 +1,39 @@
+﻿namespace DataAccess.Write
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///     A parameter for a task template
+    /// </summary>
+    public class TaskTemplateParameter
+    {
+        /// <summary>
+        ///     Identifier
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        ///     The identifier of the task template
+        /// </summary>
+        public virtual int TaskTemplateId { get; set; }
+
+        /// <summary>
+        ///     name of the parameter
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary>
+        ///     Indicates if a parameter is optional
+        /// </summary>
+        public virtual bool Optional { get; set; }
+
+        /// <summary>
+        ///     Indicates if a parameter has to be Encrypted
+        /// </summary>
+        public virtual bool Encrypted { get; set; }
+    }
+}
