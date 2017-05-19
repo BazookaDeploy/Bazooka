155cc24ed8e0097826d39a9e6f48695319cff75c
Paolo Nicodemo <paolonicodemo@gmail.com>
2017-05-19 23:16:29 +0200

    added templated tasks table

---
+++ Database/Database/Write/TemplatedTask.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[TemplatedTask](
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[EnviromentId] INT NOT NULL,
+    [Name] NVARCHAR(50) NOT NULL, 
+    [AgentId] INT NOT NULL, 
+    [CurrentlyDeployedVersion] NVARCHAR(50) NULL, 
+    [ApplicationId] INT NOT NULL, 
+    [Position] INT NOT NULL DEFAULT 1, 
+    [Deleted] BIT NOT NULL DEFAULT 0, 
+	TaskTemplateVersionId int NOT NULL,
+    CONSTRAINT [FK_TemplatedTask_Enviroments] FOREIGN KEY ([EnviromentId]) REFERENCES [Enviroments]([Id]), 
+    CONSTRAINT [FK_TemplatedTask_Applications] FOREIGN KEY (ApplicationId) REFERENCES Applications([Id]), 
+    CONSTRAINT [FK_TemplatedTask_Agents] FOREIGN KEY (AgentId) REFERENCES Agents(Id),
+	 CONSTRAINT [FK_TemplatedTask_TaskTemplate] FOREIGN KEY (TaskTemplateVersionId) REFERENCES TaskTemplateVersions(Id)
+)
---
+++ Database/Database/Write/TemplatedTaskParameters.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TemplatedTaskParameters]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	TemplatedTaskId int not null,
+	Name nvarchar(250) not null,
+	Value nvarchar(250) not null, 
+    CONSTRAINT [FK_TemplatedTaskParameters_ToTemplatedTasks] FOREIGN KEY (TemplatedTaskId) REFERENCES TemplatedTask(Id)
+)
