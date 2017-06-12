40ebeeb9307d0950d2029ef720d29ab6c5875141
Paolo Nicodemo <paolonicodemo@gmail.com>
2017-06-12 23:09:13 +0200

    added controller

---
+++ Sources/Web/Controllers/TemplatedTaskController.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.Read;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Web.Commands;
+
+namespace Web.Controllers
+{
+    public class TemplatedTaskController : BaseController
+    {
+
+        public IReadContext db { get; set; }
+
+
+        public ICollection<TemplatedTaskDto> Get()
+        {
+            return db.Query<TemplatedTaskDto>().ToList();
+        }
+
+        public TemplatedTaskDto Get(int id)
+        {
+            return db.Query<TemplatedTaskDto>().Single(X => X.Id == id);
+        }
+
+        [HttpPost]
+        public ExecutionResult CreateTemplatedTask(CreateTemplatedTask command)
+        {
+            return Execute(command);
+        }
+
+        [HttpPost]
+        public ExecutionResult RenameTemplatedTask(RenameTemplatedTask command)
+        {
+            return Execute(command);
+        }
+
+        [HttpPost]
+        public ExecutionResult Modify(ModifyTemplatedTask command)
+        {
+            return Execute(command);
+        }
+
+        [HttpPost]
+        public ExecutionResult Update(UpdateTemplatedTask command)
+        {
+            return Execute(command);
+        }
+    }
+}
