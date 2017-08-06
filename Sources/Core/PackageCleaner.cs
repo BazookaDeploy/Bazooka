c4329c53ccd3cf0d0e06cb61d147c7d7229bf166
Paolo Nicodemo <paolonicodemo@gmail.com>
2017-08-06 16:58:59 +0200

    Added new direectory to clena temp packages. Closes #85

--- Sources/Core/PackageCleaner.cs
+++ Sources/Core/PackageCleaner.cs
@@ -16,6 +16,8 @@
             MachineCache.Default.Clear();
             var dir = Path.Combine(Path.GetTempPath(), "nuget");
             Directory.Delete(dir,true);
+            var dir2 = Path.Combine(Path.GetTempPath(), "NuGetScratch");
+            Directory.Delete(dir2, true);
         }
     }
 }
