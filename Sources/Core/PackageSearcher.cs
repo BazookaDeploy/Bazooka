093e125410a2edea2b52126e61509db0ae86262f
Paolo Nicodemo <paolonicodemo@gmail.com>
2017-10-08 18:42:41 +0200

    Reducced seach only for listed pacakges

--- Sources/Core/PackageSearcher.cs
+++ Sources/Core/PackageSearcher.cs
@@ -20,7 +20,8 @@
          var factory = new PackageRepositoryFactory();
             var globalRepo = new AggregateRepository(factory, repositories.Distinct(), true);
             var packages = globalRepo.FindPackagesById(packageName);
-            return packages.Select(x => x.Version)
+            return packages.Where(x => x.Listed)
+                           .Select(x => x.Version)
                            .OrderByDescending(x => x)
                            .Select(x => x.ToString())
                            .ToList()
